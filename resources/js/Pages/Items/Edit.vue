f4b0eee32bd6836fe64ff3ac36402463f835a4c8
r-miyashita <rmiyashita338@gmail.com>
2024-02-09 16:35:02 +0900

    customers

--- resources/js/Pages/Items/Edit.vue
+++ resources/js/Pages/Items/Edit.vue
@@ -63,11 +63,11 @@ const updateItem = id => {
                                             </div>
                                             <div class="p-2 w-full">
                                                 <div class="relative">
-                                                    <label for="is_selling" class="leading-7 text-sm text-gray-600">ステータス</label>
-                                                    <input type="radio" id="is_selling" name="is_selling" v-model="form.is_selling" value="1" class="ease-in-out">
-                                                    <label class="ml-2 mr-4">販売中</label>
-                                                    <input type="radio" id="is_selling" name="is_selling" v-model="form.is_selling" value="0" class="ease-in-out">
-                                                    <label class="ml-2 mr-4">販売停止中</label>
+                                                    <label class="leading-7 text-sm text-gray-600">ステータス</label>
+                                                    <input type="radio" id="is_selling1" name="is_selling" v-model="form.is_selling" value="1" class="ease-in-out">
+                                                    <label for="is_selling1" class="ml-2 mr-4">販売中</label>
+                                                    <input type="radio" id="is_selling0" name="is_selling" v-model="form.is_selling" value="0" class="ease-in-out">
+                                                    <label for="is_selling0" class="ml-2 mr-4">販売停止中</label>
                                                 </div>
                                             </div>
                                             <div class="p-2 w-full">
